27fc9b59c80a66072160b1fe2b4910280793a4d4
Andreas Johansson <andreas.hf.johansson@gmail.com>
2015-10-14 00:00:03 +0200

    Let pid controller run at regular intervals and emit output as events

--- pid-controller.js
+++ pid-controller.js
@@ -1,8 +1,11 @@
 'use strict';
 
-class PidController {
+var events = require('events');
+
+class PidController extends events.EventEmitter {
 
   constructor(setpoint, kp, ki, kd) {
+    super();
     this.setpoint = setpoint;
     this.kp = kp;
     this.ki = ki;
@@ -13,19 +16,86 @@ class PidController {
 
     this.measuredValue = 0;
     this.output = 0;
-    this.lastTime = Date.now() - 1000;
+    this.lastTime = Date.now();
     this.errSum = 0;
     this.ITerm = 0;
     this.relayStart = 0;
+    this.loopId = 0;
   }
 
   changeSetpoint(newSetpoint) {
     this.setpoint = newSetpoint;
   }
 
+  input(newMeasuredValue) {
+    this.newMeasuredValue = newMeasuredValue;
+  }
+
+  calculate() {
+
+    if (!this.auto) {
+      this.measuredValue = this.newMeasuredValue;
+      this.lastTime = Date.now();
+      return;
+    }
+
+    // Time since last calculation
+    var now = Date.now();
+    var dTime = (now - this.lastTime) / 1000.0;
+
+    if (dTime === 0) {
+      return;
+    }
+
+    // Compute errors
+    var error = this.setpoint - this.newMeasuredValue;
+    this.ITerm += this.ki * error * dTime;
+
+    // Clamp ITerm at outputMin and outputMax to remove integral windup
+    if (this.ITerm > this.outputMax) {
+      this.ITerm = this.outputMax;
+    } else if (this.ITerm < this.outputMin) {
+      this.ITerm = this.outputMin;
+    }
+
+    var dMeasuredValue = (this.newMeasuredValue - this.measuredValue) / dTime; // Derivative on Measurement
+    //var dErr = (error - this.lastErr) / dTime; // Derivative on Error
+
+    // Compute output
+    this.output = this.kp * error + this.ITerm - this.kd * dMeasuredValue; // Derivate on Measurement
+    //this.output = this.kp * error + ki * errSum + kd * dErr; // Derivative on Error
+
+    // Clamp output, as well, since P and D can move it outside bounds
+    if (this.output > this.outputMax) {
+      this.output = this.outputMax;
+    } else if (this.output < this.outputMin) {
+      this.output = this.outputMin;
+    }
+
+    this.lastTime = now;
+    this.measuredValue = this.newMeasuredValue; // Derivative on Measurement
+    //this.lastErr = error; // Derivate on Error
+
+    // Shift relay on/off window
+    if (now - this.relayStart > this.outputMax) {
+      this.relayStart = now;
+    }
+
+    // Set relay to on if elapsed time in window is less than output
+    var relayOn = now - this.relayStart < this.output
+
+    this.emit('output', {
+      output: this.output,
+      relayOn: relayOn,
+      ITerm: this.ITerm,
+      error: error,
+      dInput: dMeasuredValue
+    });
+  }
+
   setAuto() {
     // Initialize controller when starting
-    this.lastTime = Date.now() - 1000;
+    this.lastTime = Date.now();
     this.ITerm = this.output;
 
     if (this.ITerm > this.outputMax) {
@@ -35,10 +105,14 @@ class PidController {
     }
 
     this.auto = true;
+    this.loopId = setInterval(() => {
+      this.calculate();
+    }, 1000);
   }
 
   setManual() {
     this.auto = false;
+    clearInterval(this.loopId);
   }
 
   setPID(p, i, d) {
@@ -67,62 +141,6 @@ class PidController {
       this.ITerm = this.outputMin;
     }
   }
-
-  calculate(newMeasuredValue) {
-
-
-    if (!this.auto) {
-      this.measuredValue = newMeasuredValue;
-      this.lastTime = Date.now();
-      return this.output;
-    }
-
-    // Time since last calculation
-    var now = Date.now();
-    var dTime = now - this.lastTime;
-
-    if (dTime === 0) {
-      return this.output;
-    }
-
-    // Compute errors
-    var error = this.setpoint - newMeasuredValue;
-    this.ITerm += ki * error * dTime;
-
-    // Clamp ITerm at outputMin and outputMax to remove integral windup
-    if (this.ITerm > this.outputMax) {
-      this.ITerm = this.outputMax;
-    } else if (this.ITerm < this.outputMin) {
-      this.ITerm = this.outputMin;
-    }
-
-    var dMeasuredValue = (newMeasuredValue - this.measuredValue) / dTime; // Derivative on Measurement
-    //var dErr = (error - this.lastErr) / dTime; // Derivative on Error
-
-    // Compute output
-    this.output = this.kp * error + this.ITerm - this.kd * dMeasuredValue; // Derivate on Measurement
-    //this.output = this.kp * error + ki * errSum + kd * dErr; // Derivative on Error
-
-    // Clamp output, as well, since P and D can move it outside bounds
-    if (this.output > this.outputMax) {
-      this.output = this.outputMax;
-    } else if (this.output < this.outputMin) {
-      this.output = this.outputMin;
-    }
-
-    this.lastTime = now;
-    this.measuredValue = newMeasuredValue; // Derivative on Measurement
-    //this.lastErr = error; // Derivate on Error
-
-    // Shift relay on/off window
-    if (now - this.relayStart > this.outputMax) {
-      this.relayStart = now;
-    }
-
-    // Set relay to on if elapsed time in window is less than output
-    return {
-      output: this.output,
-      relayOn: now - this.relayStart < this.output
-    };
-  }
 }
+
+module.exports = PidController;
